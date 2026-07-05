1d9a854ec1a5a8cc5071db4c43296cd5684e22b1
poonamkhevalkar <poonamkhevalkar2004@gmail.com>
2026-07-05 03:48:32 +0000

    Convertion of Datatypes

---
+++ 01_Basics/03_conversionOperation.js
@@ -0,0 +1,30 @@
+// let score = "33";
+// let score = "33abc"
+let score = true
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let valueInNumber = Number(score);
+// console.log(typeof(valueInNumber))
+
+// console.log(valueInNumber)
+
+
+// "33" => 33
+// "33abc" => NaN not a number but type of NaN number
+// true => 1 false => 0
+
+let isLoggedIn = 0;
+let boolIsLoggedIn = Boolean(isLoggedIn);
+// console.log(typeof(boolIsLoggedIn));
+// console.log(boolIsLoggedIn);
+
+// 1 => true 0 => false
+// "" => false 
+//"fj" => true
+
+
+let someNumber = 33;
+let stringNumber = String(someNumber);
+console.log(stringNumber);
+console.log(typeof(stringNumber))
