0d416378bccf74059556989efd3e1ccf2d2c218c
poonamkhevalkar <poonamkhevalkar2004@gmail.com>
2026-07-05 03:09:01 +0000

    datatypes_notes

---
+++ 01_Basics/02_datatypes.js
@@ -0,0 +1,28 @@
+"use strict" //treat all JS code as newer version
+
+// alert("2+3")  //we are using nodejs , not browser
+
+
+//code readability is more important
+console.log(3 + 3) 
+console.log('poonam')
+
+let name = "Poonam"
+let age  = 21
+let isLoggedIn = true
+let state;
+
+// number => 2 to power of 53
+// bigint
+// string => ""
+// boolean => true or false
+// null => stand alone value
+// undefined
+// symbol => use in react componenets uniqueness
+
+
+// object
+
+console.log(typeof undefined)  //undefined
+console.log(typeof null)  //object
+
---
+++ 01_Basics/notes.md
@@ -0,0 +1,15 @@
+console.log() => used to print
+console.table([variable1 ,variable2,....])
+
+# varibles & Constant
+variable changable  constant not changable
+
+we can declare variable using
+1) let    
+2) var(not worked with scope , work on entire code. if i change once it change entire value inside whole code)      
+3) direct (like accountCity = "jaipur") that not good thing
+
+we can declare costant 
+1) const
+
+
